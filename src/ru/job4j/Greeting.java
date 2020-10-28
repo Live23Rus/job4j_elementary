75e4237229425e7b37346bb100355b10654bc1ed
Владимир <Sweethome-krd@yandex.ru>
2020-10-28 22:19:03 +0300

    Площадь треугольника. [#394506]

---
+++ src/ru/job4j/Greeting.java
@@ -0,0 +1,12 @@
+package ru.job4j;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea="I like Java!";
+        int year= 2020;
+        String separate = " ";
+        System.out.println(idea+separate+year+separate+"But I am a newbie.");
+
+
+    }
+}
